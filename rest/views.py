7df9ccabb1b3ce06b5b4a61ecd2b1455e855a6f9
croach <vthakr@gmail.com>
2012-07-24 20:24:44 -0700

    Turned the validate_signature method into a function

--- rest/views.py
+++ rest/views.py
@@ -50,14 +50,11 @@ class View(DjangoView):
             handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
         else:
             handler = self.http_method_not_allowed
-        self.request = request
-        self.args = args
-        self.kwargs = kwargs
 
         # Handle request authentication
         if getattr(handler, 'auth_required', False):
             if request.GET.get('sig', False):
-                if not self._validate_signature(request, kwargs.get('domain', None)):
+                if not _validate_signature(request, *args, **kwargs):
                     return HttpResponseForbidden()
             else:
                 handler = login_required(handler)
@@ -66,58 +63,59 @@ class View(DjangoView):
         elif getattr(handler, 'admin_required', False):
             handler = user_passes_test(lambda u: u.is_superuser)(handler)
         elif getattr(handler, 'signature_required', False):
-            if not self._validate_signature(request, kwargs.get('domain', None)):
+            if not _validate_signature(request, *args, **kwargs):
                 return HttpResponseForbidden()
 
         return handler(request, *args, **kwargs)
 
 
-    def _validate_signature(self, request, domain):
-        """
-        Validates the signature associated with the given request.
-        """
+def _validate_signature(request, *args, **kwargs):
+    """
+    Validates the signature associated with the given request.
+    """
 
-        # Extract the request parameters according to the HTTP method
-        data = request.GET.copy()
-        if request.method.lower() == 'post':
-            data.update(request.POST.copy())
-        elif request.method.lower() == 'put':
-            data.update(request.PUT.copy())
+    # Extract the request parameters according to the HTTP method
+    data = request.GET.copy()
+    if request.method.lower() == 'post':
+        data.update(request.POST.copy())
+    elif request.method.lower() == 'put':
+        data.update(request.PUT.copy())
 
-        # TODO: Figure out another [good] way to get the secret key.
-        #       The problem is that this app must now know something
-        #       about the model classes that we've created, so this
-        #       framework is currently coupled to our bacon app which
-        #       not a very good design.
-        # Get the secret key for the given domain
-        try:
-            site = Site.objects.get(domain=domain)
-            secret_key = site.account.secret_key
-        except Site.DoesNotExist:
-            return False
-        except Site.MultipleObjectsReturned:
-            return False
-        except Exception:
-            return False
+    # TODO: Figure out another [good] way to get the secret key.
+    #       The problem is that this app must now know something
+    #       about the model classes that we've created, so this
+    #       framework is currently coupled to our bacon app which
+    #       not a very good design.
+    # Get the secret key for the given domain
+    try:
+        domain = kwargs.get('domain')
+        site = Site.objects.get(domain=domain)
+        secret_key = site.account.secret_key
+    except Site.DoesNotExist:
+        return False
+    except Site.MultipleObjectsReturned:
+        return False
+    except Exception:
+        return False
 
-        # Make sure the request contains a signature
-        if data.get('sig', False):
-            sig = data['sig']
-            del data['sig']
-        else:
-            return False
+    # Make sure the request contains a signature
+    if data.get('sig', False):
+        sig = data['sig']
+        del data['sig']
+    else:
+        return False
 
-        # Make sure the request contains a timestamp
-        if data.get('t', False):
-            timestamp = int(data.get('t', False))
-            del data['t']
-        else:
-            return False
+    # Make sure the request contains a timestamp
+    if data.get('t', False):
+        timestamp = int(data.get('t', False))
+        del data['t']
+    else:
+        return False
 
-        # Make sure the signature has not expired
-        delta = datetime.utcnow() - datetime.utcfromtimestamp(timestamp)
-        if delta.seconds > 5 * 60:  # If the signature is older than 5 minutes, it's invalid
-            return False
+    # Make sure the signature has not expired
+    delta = datetime.utcnow() - datetime.utcfromtimestamp(timestamp)
+    if delta.seconds > 5 * 60:  # If the signature is older than 5 minutes, it's invalid
+        return False
 
-        # Make sure the signature is valid
-        return sig == calculate_signature(secret_key, data, timestamp)
+    # Make sure the signature is valid
+    return sig == calculate_signature(secret_key, data, timestamp)
