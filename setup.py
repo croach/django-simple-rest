17dab0f28c49cf912c37a25341419900cb963797
croach <vthakr@gmail.com>
2012-07-24 18:25:22 -0700

    Adding the distribute bootstrap and setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rest',
+    version='0.1',
+    author='Christopher Roach',
+    author_email='croach@freshplum.com',
+    description='A bare bones simple package for creating RESTful APIs on top of Django',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['distribute', 'Django>=1.2']
+)
