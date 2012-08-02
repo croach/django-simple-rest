d672c8d6c50c654ba911fc0a005e594d394d3216
croach <vthakr@gmail.com>
2012-08-02 00:51:10 -0700

    Updated the RESTfulResponse class to use the wrap_object function

--- rest/response.py
+++ rest/response.py
@@ -1,4 +1,3 @@
-from functools import wraps
 import mimetypes
 
 import mimeparse
@@ -7,13 +6,15 @@ from django.conf import settings
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.core import serializers
-from django.utils.decorators import method_decorator, available_attrs
 from django.db.models.query import QuerySet
+
+from .utils.decorators import wrap_object
 
 try:
     import simplejson as json
 except ImportError:
     import json
+
 
 def serialize_json(content):
     if isinstance(content, QuerySet):
@@ -36,18 +37,29 @@ class RESTfulResponse(object):
         if mimetype_mapping:
             self.supported_mimetypes.update(mimetype_mapping)
 
-
     def __call__(self, view_obj):
-        def wrapper(resource, request, *args, **kwargs):
-            context_dict = view_obj(resource, request, *args, **kwargs)
-            response = self._create_response(request, context_dict)
-            return response
+        def decorator(view_func):
+            def wrapper(request, *args, **kwargs):
+                results = view_func(request, *args, **kwargs)
+                try:
+                    context_dict, status_code = results
+                except ValueError:
+                    context_dict, status_code = results, 200
+                # TODO: What about a view that returns a normal Django
+                #       HttpResponse object? Should we allow this to error
+                #       out when one is encountered? Should we just allow it
+                #       to propogate through (i.e., kind of an override)? Or,
+                #       should we fectch the content from it and pass that to
+                #       render_to_response method?
+                response = self.render_to_response(request, context_dict, status_code)
+                return response
+            return wrapper
+        return wrap_object(view_obj, decorator)
 
-        return wrapper
-
-    def _create_response(self, request, context_dict):
+    def render_to_response(self, request, context_dict=None, status=200):
         mimetype = mimeparse.best_match(self.supported_mimetypes.keys(), request.META['HTTP_ACCEPT'])
-        # mimetype = mimetypes.guess_type(request.path_info)[0] || mimetype
+        import ipdb; ipdb.set_trace()
+        mimetype = mimetypes.guess_type(request.path_info)[0] or mimetype
         content_type = '%s; charset=%s' % (mimetype, settings.DEFAULT_CHARSET)
 
         templ_or_func = self.supported_mimetypes.get(mimetype)
@@ -55,14 +67,18 @@ class RESTfulResponse(object):
             templ_or_func = self.supported_mimetypes[DEFAULT_MIMETYPE]
 
         if isinstance(templ_or_func, str):
-            def serializer(context_dict):
+            def serialize(context_dict):
+                context_dict = context_dict or {}
                 response = render_to_response(templ_or_func, context_dict)
                 response['Content-Type'] = content_type
+                response.status_code = status
                 return response
         else:
-            def serializer(context_dict):
-                serializer = self.supported_mimetypes[mimetype]
-                response = HttpResponse(serializer(context_dict), content_type=content_type)
+            def serialize(context_dict):
+                if context_dict:
+                    response = HttpResponse(templ_or_func(context_dict), content_type=content_type, status=status)
+                else:
+                    response = HttpResponse(content_type=content_type, status=status)
                 return response
 
-        return serializer(context_dict)
+        return serialize(context_dict)
